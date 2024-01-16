eeb7e06c7c326014306b152080cc470c7510be33
최민우 <cmw0918@naver.com>
2024-01-16 20:53:47 +0900

    feat: login 퍼블리싱

---
+++ just-plan/src/app/login/page.tsx
@@ -0,0 +1,19 @@
+import { LoginForm } from "@/components/LoginForm";
+import Image from "next/image";
+
+export default function Login() {
+  return (
+    <div className="w-full h-dvh ">
+      <div className="flex h-full gap-4 justify-center items-center">
+        <Image
+          className="md:block hidden"
+          src={"/images/LoginImage.png"}
+          alt={""}
+          width={300}
+          height={300}
+        />
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
