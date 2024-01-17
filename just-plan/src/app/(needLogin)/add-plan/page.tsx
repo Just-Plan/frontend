0d169219710d537f44cfe9ba67238f0dccc58352
KangYunJi <dbswl701@ajou.ac.kr>
2024-01-17 21:31:41 +0900

    design: 일정 등록 상단 퍼블리싱

--- just-plan/src/app/(needLogin)/add-plan/page.tsx
+++ just-plan/src/app/(needLogin)/add-plan/page.tsx
@@ -1,45 +1,61 @@
 import DayPlanCard from "@/components/DayPlanCard/DayPlanCard";
+import { Button } from "@/components/ui/Button";
 import Image from "next/image";
 
 const page = () => {
   return (
-    <div>
-      <div>
-        <div className="flex">
+    <div className="m-10">
+      <div className="">
+        <div className="flex items-center">
           <Image
             src="/airplane.png"
             width={25}
             height={25}
             alt="비행기 아이콘"
           />
-          <div>제주도</div>
-          <div>2024-01-05~2024-02-05</div>
+          <div className="ml-2">제주도</div>
+          <div className="text-xs ml-28">2024-01-05~2024-02-05</div>
         </div>
-        <div className="font-bold text-3xl">강윤지님의 제주도 먹고뿌셔</div>
-        <div className="text-cyan-600 font-bold"># 해시태그 # 해시태그</div>
-        <div className="flex justify-between">
-          <div className="flex">
-            <div className="bg-emerald-100 flex p-2 px-5 rounded-full items-center">
-              <Image src="/cash.png" alt="현금" width={30} height={30} />
-              <div className="ml-2 text-neutral-700">200,000₩</div>
+        <div className="flex">
+          <div className="flex items-center flex-1">
+            <div className="font-bold text-3xl my-3 mr-5">
+              강윤지님의 제주도 먹고뿌셔
             </div>
-            <div className="bg-emerald-100 flex p-2 px-5 rounded-full items-center">
-              <Image src="/card.png" alt="카드" width={30} height={30} />
-              <div className="ml-2 text-neutral-700">500,000₩</div>
-            </div>
-            <div className="border-2 border-emerald-100 flex p-2 px-5 rounded-full items-center">
-              <Image src="/cash.png" alt="현금" width={30} height={30} />
-              <div className="ml-2 text-neutral-700">200,000₩</div>
+            <div className="hover:cursor-pointer rounded-full p-1 hover:bg-gray-200">
+              <Image src="/images/edit.png" alt="수정" width={27} height={27} />
             </div>
           </div>
 
-          <div className="float: right flex">
-            <button>저장</button>
+          <div className="flex items-center hover:cursor-pointer rounded-full p-1 w-10 h-10 hover:bg-gray-200">
+            <Image src="/images/map.png" alt="지도" width={40} height={40} />
+          </div>
+        </div>
+
+        <div className="text-cyan-600 font-bold"># 해시태그 # 해시태그</div>
+        <div className="flex justify-between items-center">
+          <div className="flex gap-5 my-5">
+            <div className="bg-ourGreen/60 flex p-2 px-5 rounded-full items-center">
+              <Image src="/cash.png" alt="현금" width={30} height={30} />
+              <div className="ml-2 text-neutral-700">200,000₩</div>
+            </div>
+            <div className="bg-ourGreen/60 flex p-2 px-5 rounded-full items-center">
+              <Image src="/card.png" alt="카드" width={30} height={30} />
+              <div className="ml-2 text-neutral-700">500,000₩</div>
+            </div>
+            <div className="border-2 border-ourGreen/60 flex p-2 px-5 rounded-full items-center">
+              <Image src="/cash.png" alt="현금" width={30} height={30} />
+              <div className="ml-2 text-neutral-700">200,000₩</div>
+            </div>
+          </div>
+          <div>
+            <Button variant="outline" className="w-28">
+              저장
+            </Button>
           </div>
         </div>
       </div>
-      <div className="bg-emerald-200 flex flex-col">
-        <div className=" bg-white m-5 h-20 rounded-2xl mt10 flex gap-10 items-center px-8 font-bold overflow-y-scroll">
+      <div className="bg-ourGreen flex flex-col p-5 rounded-2xl">
+        <div className=" bg-white h-20 rounded-2xl mb-5 flex gap-10 items-center px-8 font-bold overflow-y-scroll">
           <div>ALL</div>
           <div className="flex flex-col items-center">
             <div>Day1</div>
