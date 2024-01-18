c2bbf2401c54ced3d99a8bf13296c0cdeb119eed
KangYunJi <dbswl701@ajou.ac.kr>
2024-01-18 16:17:08 +0900

    feat: 일정 메모 작성 및 배경색 변경 기능 구현

--- just-plan/src/app/(needLogin)/_components/MemoModal/MemoModal.tsx
+++ just-plan/src/app/(needLogin)/_components/MemoModal/MemoModal.tsx
@@ -1,30 +1,45 @@
+"use client";
+
 import { Button } from "@/components/ui/Button";
 import {
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { memoBGList } from "@/constants";
+import { useState } from "react";
 
 const MemoModal = () => {
+  const [bg, setBG] = useState("bg-white");
+
   return (
     <DialogContent className="sm:max-w-[550px]">
       <DialogHeader>
-        <DialogTitle>메모</DialogTitle>
+        <DialogTitle className="text-2xl mb-3">메모</DialogTitle>
         <DialogDescription className="flex justify-between border-t-2 border-b-2 py-1">
-          <div className="bg-white w-14 h-8 rounded-3xl border hover:cursor-pointer"></div>
-          <div className="bg-[#FEE4E3] w-14 h-8 rounded-3xl hover:cursor-pointer"></div>
-          <div className="bg-[#E3F5F6] w-14 h-8 rounded-3xl hover:cursor-pointer"></div>
-          <div className="bg-[#F0F7D9] w-14 h-8 rounded-3xl hover:cursor-pointer"></div>
-          <div className="bg-[#FFF3CC] w-14 h-8 rounded-3xl hover:cursor-pointer"></div>
-          <div className="bg-[#F6D9FE] w-14 h-8 rounded-3xl hover:cursor-pointer"></div>
-          <div className="bg-[#D0CFFF] w-14 h-8 rounded-3xl hover:cursor-pointer"></div>
+          {memoBGList.map((item) => (
+            <div
+              onClick={() => setBG(item)}
+              className={`${item} w-14 h-8 rounded-3xl hover:cursor-pointer ${
+                item === "bg-white" ? "border" : ""
+              }`}
+              key={item}
+            ></div>
+          ))}
         </DialogDescription>
       </DialogHeader>
-      <div className="bg-red-100 flex h-96 p-5">asdfasdf</div>
+      <textarea
+        className={`flex h-96 p-5 resize-none outline-none text-xl ${bg} ${
+          bg === "bg-white" ? "border" : ""
+        }`}
+      />
       <DialogFooter className="m-auto">
-        <Button type="submit">저장하기</Button>
+        <DialogClose asChild>
+          <Button type="submit">저장하기</Button>
+        </DialogClose>
       </DialogFooter>
     </DialogContent>
   );
