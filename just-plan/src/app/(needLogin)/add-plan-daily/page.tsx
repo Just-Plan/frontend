e16c04ce80f7c287ea3c4487acb4c250f79733bb
KangYunJi <dbswl701@ajou.ac.kr>
2024-01-24 15:27:47 +0900

    design: 일정 등록 일차별 페이지 모바일뷰 퍼블리싱

--- just-plan/src/app/(needLogin)/add-plan-daily/page.tsx
+++ just-plan/src/app/(needLogin)/add-plan-daily/page.tsx
@@ -10,9 +10,8 @@ import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import EditPlanInfoModal from "../_components/EditPlanInfoModal/EditPlanInfoModal";
 import type { IPlanInfo } from "@/types/plan.types";
 import { PlanDayHeader } from "../_components/PlanDayHeader/PlanDayHeader";
-import { AddedPlaceCard, StoredPlaceCard } from "@/components";
-import MemoModal from "../_components/MemoModal/MemoModal";
-import { AddPlaceModal } from "../_components";
+import { StoredPlaceCard } from "@/components";
+import { AddPlaceModal, StoredPlaceMiniCard } from "../_components";
 
 const Page = () => {
   const [info, setInfo] = useState<IPlanInfo>(PlanInfo);
@@ -67,7 +66,7 @@ const Page = () => {
         <ShowMoney cache={cache} card={card} />
       </div>
       <div className="bg-ourGreen flex flex-row p-3 sm:p-5 rounded-2xl gap-5">
-        <div className="bg-white rounded-2xl p-5">
+        <div className="bg-white rounded-2xl p-5 hidden sm:block">
           <div className="mb-5 flex justify-between">
             <div className="text-2xl font-bold ">장소 보관함</div>
             <Dialog>
@@ -89,9 +88,14 @@ const Page = () => {
         </div>
         <div className="flex flex-col w-full">
           <PlanDayHeader days={Plan} />
+          <div className="flex bg-white mb-3 p-2 gap-3 rounded-xl overflow-x-auto sm:hidden">
+            {StoredPlace.map((item) => (
+              <StoredPlaceMiniCard key={item.id} place={item} />
+            ))}
+          </div>
           <div className="flex gap-5">
             <DayPlanCard item={Plan[0]} />
-            <div className="bg-white w-full">지도</div>
+            <div className="bg-white w-full hidden sm:block">지도</div>
           </div>
         </div>
       </div>
