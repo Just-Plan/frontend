3ff4f03599b249fa60df4337fc4c4e6e3099293e
KangYunJi <dbswl701@ajou.ac.kr>
2024-01-25 14:14:03 +0900

    fix: 일정 일차별-장소보관함 dnd 이동시간까지 이동되는 버그 수정

--- just-plan/src/components/AddedPlaceCard/AddedPlaceCardDnD.tsx
+++ just-plan/src/components/AddedPlaceCard/AddedPlaceCardDnD.tsx
@@ -22,14 +22,11 @@ export const AddedPlaceCardDnD = ({ item, provided, snapshot }: IProps) => {
     ref={provided.innerRef}
     {...provided.draggableProps}
     {...provided.dragHandleProps}
-    className={cn(
-      snapshot.isDragging
-        ? "bg-opacity-90 shadow-2xl shadow-gray-400"
-        : ""
-    )}
   >
     <Dialog>
-      <DialogTrigger asChild className="hover:cursor-pointer">
+      <DialogTrigger asChild className={cn("hover:cursor-pointer", snapshot.isDragging
+        ? "bg-opacity-90 shadow-2xl shadow-gray-400"
+        : "")}>
         <div className="flex relative flex-col w-full hover:cursor-pointer">
           <div className="border w-[280px] sm:w-80 rounded-xl flex p-3 z-10 bg-white">
             <div className="w-16 h-16 relative my-auto">
@@ -62,7 +59,7 @@ export const AddedPlaceCardDnD = ({ item, provided, snapshot }: IProps) => {
             </div>
             <div className="float-right">x</div>
           </div>
-          {time && (
+          {time && !snapshot.isDragging && (
             <div className="flex items-center justify-center h-10 relatvie">
               <div className="border h-10 w-px absolute left-1/2 border-dashed" />
               <div className="z-10 bg-white text-xs text-zinc-500 hover:cursor-pointer">
--- just-plan/src/components/DayPlanCard/DayPlanCardDnD.tsx
+++ just-plan/src/components/DayPlanCard/DayPlanCardDnD.tsx
@@ -23,66 +23,52 @@ export interface IProps {
 const DayPlanCardDnD = ({ item, items, setItems }: PropsWithChildren<IProps>) => {
   const { id, date, image, title, category, address } = item;
 
-  // 요기를 modify 거기로 이동시키자.
-  // const [items, setItems] = useState<ITems>({
-  //   stored: [],
-  //   added: Plan,
-  // });
-
   return (
-    // <DragDropContext onDragEnd={onDragEnd}>
+    <div className="bg-white flex flex-col w-fit p-6  rounded-3xl">
+      <div className="flex justify-between">
+        <div>
+          <div className="font-bold text-2xl text-slate-400">{id}일차</div>
+          <div className="text-slate-400 text-sm font-bold">{date}</div>
+        </div>
 
-          
-        <div className="bg-white flex flex-col w-fit p-6  rounded-3xl">
-          <div className="flex justify-between">
-            <div>
-              <div className="font-bold text-2xl text-slate-400">{id}일차</div>
-              <div className="text-slate-400 text-sm font-bold">{date}</div>
-            </div>
-
-            <div className="flex">
-              <Switch id={id.toString()} />
-              <label htmlFor={id.toString()} className="ml-3">
-                대중교통
-              </label>
-            </div>
-          </div>
-          <Droppable droppableId={"added"}>
-      {(provided, snapshot) => (
-        <div
-        ref={provided.innerRef}
-        {...provided.droppableProps}
-        className={cn(
-          "flex flex-col gap-5",
-          snapshot.isDraggingOver ? "shadow-lg" : ""
-        )}
-      >
-          <div className="flex flex-col items-center h-[600px] w-full overflow-y-scroll relative">
+        <div className="flex">
+          <Switch id={id.toString()} />
+          <label htmlFor={id.toString()} className="ml-3">
+            대중교통
+          </label>
+        </div>
+      </div>
+      <Droppable droppableId={"added"}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className={cn(
+              "flex flex-col gap-5",
+              snapshot.isDraggingOver ? "shadow-lg" : ""
+            )}
+          >
+            <div className="flex flex-col items-center h-[600px] w-full overflow-y-scroll relative">
             {items["added" as TItemStatus].map((item, index) => (
               <Draggable
-              key={item.id}
-              draggableId={item.id.toString()}
-              index={index}
-            >
-              {(provided, snapshot) => (
+                key={item.id}
+                draggableId={item.id.toString()}
+                index={index}
+              >
+                {(provided, snapshot) => (
                   <AddedPlaceCardDnD key={item.id} item={item} provided={provided} snapshot={snapshot} />
-              )}
-            </Draggable>
+                )}
+              </Draggable>
             ))}
-
-              {provided.placeholder}
-
+            {provided.placeholder}
             {/* {Plan.map((item) => (
               <AddedPlaceCard key={item.id} item={item} />
             ))} */}
+            </div>
           </div>
-        </div>
-              )}
-              </Droppable>
-          
-      </div>
-    // </DragDropContext>
-
+        )}
+      </Droppable>
+    </div>
   );
 };
 
