c3c51fe564820ac47958f23a022af5b31006b69b
KangYunJi <dbswl701@ajou.ac.kr>
2024-02-19 17:58:10 +0900

    feat: 회원 정보 불러오기

--- just-plan/src/app/(needLogin)/mypage/_components/leftSide.tsx
+++ just-plan/src/app/(needLogin)/mypage/_components/leftSide.tsx
@@ -1,11 +1,27 @@
 "use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/Avatar";
+import { useGetUserInfo } from "@/hooks/useGetUserInfo";
 import { localStorageUserInfoAtom } from "@/store/auth.atom";
 import { Label } from "@radix-ui/react-label";
-import { useAtomValue } from "jotai";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
 
 const LeftSide = () => {
-  const userInfo = useAtomValue(localStorageUserInfoAtom);
+  const [userInfo, setUserInfo] = useAtom(localStorageUserInfoAtom);
+  // 회원 정보 조회
+  const {
+    data: userInfoData,
+    error: userInfoError,
+    isLoading: userInfoIsLoading,
+  } = useGetUserInfo();
+
+  useEffect(() => {
+    if (userInfoData) {
+      setUserInfo(userInfoData);
+    }
+  }, [userInfoData]);
+  if (userInfoError) return <div>에러</div>;
+  if (userInfoIsLoading) return <div>로딩중</div>;
 
   return (
     <div className="flex flex-col justify-center items-center px-5 gap-5">
@@ -14,17 +30,19 @@ const LeftSide = () => {
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
       <div className="flex justify-center flex-col items-center ">
-        <p>{userInfo.name}</p>
-        <p className="font-extralight text-sm">{userInfo.email}</p>
+        <p>{userInfoData?.name}</p>
+        <p className="font-extralight text-sm">{userInfoData?.email}</p>
       </div>
-      <p className="text-center">INFP 윤지의 여행 일정 기록장임뮈당</p>
+      <p className="text-center">
+        {userInfoData?.introduction || "소개글을 입력해보세요!"}
+      </p>
       <div className="flex justify-center flex-col items-center ">
         <Label>총 게시글 수</Label>
-        <p>14</p>
+        <p>{userInfo?.totalUserPlan}</p>
       </div>
       <div className="flex justify-center flex-col items-center ">
         <Label>총 스크랩 수</Label>
-        <p>52</p>
+        <p>{userInfo?.totalScrap}</p>
       </div>
     </div>
   );
