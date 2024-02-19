59f14564c90699979d1e634736a2f15b249a761f
KangYunJi <dbswl701@ajou.ac.kr>
2024-02-20 02:02:02 +0900

    fix: 회원정보 수정 기능 수정

--- just-plan/src/app/(needLogin)/mypage/_lib/patchEditInfo.ts
+++ just-plan/src/app/(needLogin)/mypage/_lib/patchEditInfo.ts
@@ -1,12 +1,21 @@
 import { nextFetch } from "@/lib/returnFetch";
+import type { UserInfo } from "@/store/auth.atom.type";
 
 // 수정 필요
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const patchEditInfo = async (info: any) => {
-  const res = await nextFetch("/api/user/update", {
+export const patchEditInfo = async ({
+  name,
+  mbtiName,
+  introduction,
+}: {
+  name: string;
+  mbtiName: string;
+  introduction: string;
+}) => {
+  const res = await nextFetch<UserInfo>("/api/user/update", {
     method: "PATCH",
-    body: JSON.stringify(info),
+    body: JSON.stringify({ name, mbtiName, introduction }),
   });
 
-  return res.json();
+  return res;
 };
